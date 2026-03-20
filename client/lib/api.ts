77093d32819f23acc570e653fc80eaa1147b14b3
Yonas Teshome <yoyoni826@gmail.com>
2026-03-20 18:12:31 +0300

    frontend is modified

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vehicle-rental-system-wrjt.vercel.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
